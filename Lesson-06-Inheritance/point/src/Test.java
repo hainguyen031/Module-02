5d02a3e95f172b5631409044a594ed04348381fd
hainguyen031 <129949149+hainguyen031@users.noreply.github.com>
2023-05-16 16:26:48 +0700

    point

---
+++ Lesson-06-Inheritance/point/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        Point2D test = new Point2D();
+        System.out.println(test);
+
+        Point2D test1 = new Point2D(3,4);
+        System.out.println(test1);
+
+        Point3D test2 = new Point3D();
+        System.out.println(test2);
+
+        Point3D test3 = new Point3D(3,4,5);
+        System.out.println(test3);
+    }
+}
