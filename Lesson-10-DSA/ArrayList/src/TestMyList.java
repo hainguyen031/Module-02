ba53f3ae94a2159f401a4e7eda10cb2141e7184a
hainguyen031 <129949149+hainguyen031@users.noreply.github.com>
2023-05-20 17:06:40 +0700

    done

--- Lesson-10-DSA/ArrayList/src/TestMyList.java
+++ Lesson-10-DSA/ArrayList/src/TestMyList.java
@@ -1,33 +1,16 @@
 public class TestMyList {
     public static void main(String[] args) {
-        Student khoa = new Student(1, "Khoa", "22/12", "C12");
-        Student trung = new Student(2, "Trung", "22/12", "C12");
-        Student giang = new Student(3, "Giang", "22/12", "C12");
-        Student luat = new Student(4, "Luat", "22/12", "C12");
-
-        MyList<Student> studentMyList = new MyList<>();
-        studentMyList.add(khoa);
-        studentMyList.add(trung);
-        studentMyList.add(0, giang);
-        studentMyList.add(luat);
-
-        System.out.println("Name of Student in student list");
-        for (int i = 0; i < studentMyList.size(); i++) {
-            System.out.println(studentMyList.get(i).getName());
+        MyList myList=new MyList<>();
+        myList.add(1,"1");
+        myList.add(2,"2");
+        myList.add(3,"3");
+        myList.add(4,"4");
+        myList.add(5,"5");
+        System.out.println(myList.remove(3));
+        System.out.println(myList.size());
+        myList.clear();
+        for (int i=0;i<myList.size();i++){
+            System.out.println(myList.get(i));
         }
-        Student studentRemove = studentMyList.remove(1);
-        System.out.println("Remove and return student at index 1: " + studentRemove);
-        System.out.println("Student list after remove");
-        for (int i = 0; i < studentMyList.size(); i++) {
-            System.out.println(studentMyList.get(i).getName());
-        }
-
-        System.out.println("index of Trung in list " + studentMyList.indexOf(trung));
-
-        System.out.println("Is student list contain Khoa: " + studentMyList.contains(khoa));
-
-        System.out.println("Clear all student in list:");
-        studentMyList.clear();
-        System.out.println("Size of student list after clear " + studentMyList.size());
     }
 }
